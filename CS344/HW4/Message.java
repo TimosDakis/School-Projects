135d2e6187e312cd22a2a5ee5d8e4b47d50872ef
TimosDakis <85649514+TimosDakis@users.noreply.github.com>
2022-12-26 22:47:12 -0500

    Create Message.java

---
+++ CS344/HW4/Message.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Message implements Serializable {
+	int num;
+	int id;
+	
+	public Message(int num, int id) {
+		this.num = num;
+		this.id = id;
+	} // end constructor
+	
+} // end Message class
